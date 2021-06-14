d0f3912b1c43055421aa0fbc61df081e6dfef643
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-14 16:58:34 +0200

    push exercices PHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projet05 Exercice02</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid col-11 p-3 m-4 border border-warning rounded justify-content-center">
+        <div class="row ">
+            <h1 class="col-12 text-secondary text-center">PHP Projet05 Exercice02</h1>
+            <h2 class="col-12 text-info text-center">Créez un lien avec les paramètres "nom", "prénom" et "âge" dirigeant sur une autre page et affichez ces paramètres.</h2>
+        </div>
+        <div class="row border border-primary rounded p-3 m-4 text-secondary justify-content-center">
+            <a href="index02.php?nom=Cécile&amp;prenom=Cécile&amp;age=41">cliquez pour être redirigé vers une page qui affiche les paramètres de l'url : </a>
+        </div>
+        <div class="row border border-primary rounded p-3 m-4 text-secondary justify-content-center">
+            <?php 
+                echo 'Nom : '. $_GET['nom']. ' '. 'Prénom : '. $_GET['prenom']. '  '. 'Âge : '. '  '. $_GET['age'];
+            ?>
+        </div>
+    </div>
+</body>
+</html>
